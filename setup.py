976dc2c370ae2149f4cbde4d20cc62aa95c0b5a2
pattonw <pattonw@hhmi.org>
2022-10-19 09:25:09 -0700

    add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="funlib.geometry",
     version="0.1",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/funkelab/funlib.geometry",
     author="Jan Funke",
     author_email="funkej@janelia.hhmi.org",
