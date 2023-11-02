2e0c1c73e7a105ad9ba093a44915b1b60c20a3e7
William Patton <wllmpttn24@gmail.com>
2023-11-01 20:40:23 -0700

    add version info to package

--- funlib/geometry/__init__.py
+++ funlib/geometry/__init__.py
@@ -1,2 +1,8 @@
 from .coordinate import Coordinate  # noqa
 from .roi import Roi  # noqa
+
+__major__ = 0
+__minor__ = 2
+__patch__ = 0
+__tag__ = ""
+__version__ = "{}.{}.{}{}".format(__major__, __minor__, __patch__, __tag__).strip(".")
