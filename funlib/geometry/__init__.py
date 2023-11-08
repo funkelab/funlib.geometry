ece120622be83947f3e7af2dde5373afe29f1699
pattonw <pattonw@hhmi.org>
2023-11-08 07:32:44 -0800

    set version to 0.3

--- funlib/geometry/__init__.py
+++ funlib/geometry/__init__.py
@@ -1,8 +1,8 @@
 from .coordinate import Coordinate  # noqa
 from .roi import Roi  # noqa
 
-__major__ = 1
-__minor__ = 0
+__major__ = 0
+__minor__ = 3
 __patch__ = 0
 __tag__ = ""
 __version__ = "{}.{}.{}{}".format(__major__, __minor__, __patch__, __tag__).strip(".")
